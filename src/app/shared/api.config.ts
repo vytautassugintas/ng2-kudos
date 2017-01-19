2c0a83b12e9de0088ad28b17fc634141fa4e9b0b
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2017-01-19 21:36:17 +0200

    Refactoring services

--- src/app/shared/api.config.ts
+++ src/app/shared/api.config.ts
@@ -1,5 +1,8 @@
 export class API {
-    //public static readonly URL = 'http://212.24.96.238:8080/open-kudos-0.1.0/';
-    public static readonly URL = 'https://test.openkudos.com/api/';
-    //public static readonly URL = 'http://localhost:8080/';
+    public static readonly URL = "https://test.openkudos.com/api/";
+    // Entrypoints
+    public static readonly ENTRY = {
+      AUTHENTICATION : API.URL + "authentication",
+      CHALLENGE : API.URL + "challenge"
+    }
 }
