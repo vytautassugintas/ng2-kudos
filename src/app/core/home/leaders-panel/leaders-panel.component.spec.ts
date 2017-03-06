9799978f721e26bbaa1d542f1eb3d7eda07d07aa
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2017-03-06 22:56:22 +0200

    test: specs for leaders panel component

--- src/app/core/home/leaders-panel/leaders-panel.component.spec.ts
+++ src/app/core/home/leaders-panel/leaders-panel.component.spec.ts
@@ -1,9 +1,9 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
-import { LeadersPanelComponent } from './leaders-panel.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {LeadersPanelComponent} from './leaders-panel.component';
+import {RouterLinkStubDirective} from "../../../../test/router-stubs";
+import {LeadersService} from "../../../shared/services/leaders.service";
+import {Observable} from "rxjs";
 
 describe('LeadersPanelComponent', () => {
   let component: LeadersPanelComponent;
@@ -11,9 +11,16 @@ describe('LeadersPanelComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LeadersPanelComponent ]
+      declarations: [LeadersPanelComponent, RouterLinkStubDirective]
     })
-    .compileComponents();
+      .overrideComponent(LeadersPanelComponent, {
+        set: {
+          providers: [
+            {provide: LeadersService, useClass: LeadersServiceSpy}
+          ]
+        }
+      })
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -25,4 +32,28 @@ describe('LeadersPanelComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should get top receivers on init', () => {
+    expect(component.leaders).toEqual([{fullName: "test test", kudosAmount: 1, userId: "id123"}])
+  })
+
+  it('should get top senders on selection', () => {
+    component.selectTab("SENDERS");
+    expect(component.leaders).toEqual([{fullName: "sender tester", kudosAmount: 2, userId: "312id"}])
+  })
+
+  it('should get top receivers on selection', () => {
+    component.selectTab("RECEIVERS");
+    expect(component.leaders).toEqual([{fullName: "test test", kudosAmount: 1, userId: "id123"}])
+  })
 });
+
+class LeadersServiceSpy {
+  getTopReceivers(): Observable<any> {
+    return Observable.of([{fullName: "test test", kudosAmount: 1, userId: "id123"}]);
+  }
+
+  getTopSenders(): Observable<any> {
+    return Observable.of([{fullName: "sender tester", kudosAmount: 2, userId: "312id"}]);
+  }
+}
