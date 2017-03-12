c8b29dd9a07ad71c6d34b8ba408bbaccfcd1511d
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2017-03-12 21:41:38 +0200

    feat: user can add endorsement when giving kudos

--- src/app/core/home/give/give.component.ts
+++ src/app/core/home/give/give.component.ts
@@ -4,6 +4,7 @@ import {KudosService} from "../../../shared/services/kudos.service";
 import {NotificationsService} from "angular2-notifications/lib/notifications.service";
 import {UserService} from "../../../shared/services/user.service";
 import {NotificationService} from "../../../shared/components/notification/notification.service";
+import {ENDORSEMENT} from "../../../shared/endorsements.constant";
 
 @Component({
   selector: 'app-give',
@@ -15,38 +16,48 @@ export class GiveComponent implements OnInit {
 
   isReady: boolean;
   isExpanded: boolean;
+  isModalOpened: boolean;
   hasErrors: boolean;
   errorMessage: string;
   formModel: GiveKudosFormModel;
+  endorsements: Array<any>;
+  endorsementSearchValue: string = "";
+  selectedEndorsement: string = "";
 
   constructor(private kudosService: KudosService, private userService: UserService, private notificationService: NotificationService) {
     this.isReady = true;
-    this.isExpanded = false;
+    this.isExpanded = true;
+    this.isModalOpened = false;
     this.hasErrors = false;
     this.errorMessage = null;
     this.formModel = new GiveKudosFormModel();
+    this.endorsements = ENDORSEMENT.LIST;
   }
 
   ngOnInit() {
-    this.isExpanded = false;
-    this.isReady = true;
+
   }
 
   onSubmit() {
-    this.clearErrors();
-    this.kudosService.give(this.formModel).subscribe(response => {
-      this.notificationService.success("Success", response.amount + " kudos sent to " + response.receiverFullName);
-      this.userService.currentUser.weeklyKudos -= this.formModel.amount;
-      this.userService.currentUser.experiencePoints += this.formModel.amount * 2;
-      this.userService.updateUser(this.userService.currentUser);
-      this.clearForm();
-    }, error => {
-      console.log(error);
-      if (error.fieldError) {
-        this.hasErrors = true;
-        this.errorMessage = error.fieldError.message;
-      }
-    })
+    if (!this.formModel.isValid()) {
+      this.hasErrors = true;
+      this.errorMessage = "One of the fields is empty";
+    } else {
+      this.clearErrors();
+      this.kudosService.give(this.formModel).subscribe(response => {
+        this.notificationService.success("Success", response.amount + " kudos sent to " + response.receiverFullName);
+        this.userService.currentUser.weeklyKudos -= this.formModel.amount;
+        this.userService.currentUser.experiencePoints += this.formModel.amount * 2;
+        this.userService.updateUser(this.userService.currentUser);
+        this.clearForm();
+      }, error => {
+        console.log(error);
+        if (error.fieldError) {
+          this.hasErrors = true;
+          this.errorMessage = error.fieldError.message;
+        }
+      })
+    }
   }
 
   onClearClick() {
@@ -58,6 +69,10 @@ export class GiveComponent implements OnInit {
     this.isExpanded === false ? this.isExpanded = true : this.isExpanded = false;
   }
 
+  toggleModal() {
+    this.isModalOpened === false ? this.isModalOpened = true : this.isModalOpened = false;
+  }
+
   clearForm() {
     this.formModel = new GiveKudosFormModel();
   }
@@ -65,6 +80,37 @@ export class GiveComponent implements OnInit {
   clearErrors() {
     this.hasErrors = false;
     this.errorMessage = "";
+  }
+
+  searchItems(ev) {
+    this.initializeItems();
+    let val = ev;
+
+    if (val && val.trim() != '') {
+      let arrayToCheck: Array<string> = [];
+      for (let i in this.endorsements){
+        for(let item of this.endorsements[i].endorsements){
+          arrayToCheck.push(item.endorsement);
+        }
+      }
+
+      this.endorsements = arrayToCheck.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
+  selectEndorsement(endorsement){
+    this.formModel.endorsement = endorsement;
+    this.toggleModal();
+  }
+
+  clearEndorsement(){
+    this.formModel.endorsement = "";
+  }
+
+  initializeItems() {
+    this.endorsements = ENDORSEMENT.LIST;
   }
 
 }
--- src/app/shared/models/GiveKudosFormModel.ts
+++ src/app/shared/models/GiveKudosFormModel.ts
@@ -2,12 +2,24 @@ export class GiveKudosFormModel{
   receiverEmail: string;
   amount: number;
   message: string;
+  endorsement: string;
 
   toJSON(){
     return {
       receiverEmail: this.receiverEmail,
       amount: this.amount,
-      message: this.message
+      message: this.message,
+      endorsement: this.endorsement
+    }
+  }
+
+  isValid(){
+    if (!this.receiverEmail.length){
+      return false;
+    } else if (this.amount < 0){
+      return false;
+    } else {
+      return true;
     }
   }
 
